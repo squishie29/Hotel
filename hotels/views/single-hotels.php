e9f5b4dae099bbbf016d47aec35cbad14425688e
Guesmi Rami <47742394+squishie29@users.noreply.github.com>
2019-12-04 13:20:43 +0100

    jawna behi just nrigl el style mte3 el tab

--- hotels/views/single-hotels.php
+++ hotels/views/single-hotels.php
@@ -43,7 +43,7 @@ include "../core/chambreC.php";
 	$hotel1C=new hotelC();
     $hotel2C=new hotelC();  
     $chambre1C=new chambreC();
-    $result3=$chambre1C->afficherchambres(); 
+    $result3=$chambre1C->recupererchambre($_POST['idH']); 
     $result2=$hotel1C->afficherhotels();
     $result=$hotel1C->recupererhotel($_POST['idH']);
 	foreach($result as $row)
