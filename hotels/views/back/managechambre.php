62f8828634f72e9cc99a41b6264e2f88bbcfdf48
Guesmi Rami <47742394+squishie29@users.noreply.github.com>
2019-12-12 21:48:30 +0100

    iuyt

--- hotels/views/back/managechambre.php
+++ hotels/views/back/managechambre.php
@@ -1,10 +1,4 @@
-<?php
-// On démarre la session (ceci est indispensable dans toutes les pages de notre section membre)
-session_start ();  
- 
-// On récupère nos variables de session
-if (isset($_SESSION['l']) && isset($_SESSION['p'])) 
-{ ?>
+
 <?php  
 $servername='localhost';
 $username='root';
@@ -212,12 +206,3 @@ echo $pagLink . "</ul>";
     </div>     
 </body>
 </html>        
-<?php 
-
-}
-
-else { 
-      echo 'Veuillez vous connecter </br>';  
-	  echo '<a href="auth.html">Cliquer pour se connecter</a>';
-
-}  ?>
